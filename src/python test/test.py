52e089aa07c9074fd17986acf3fde920f9f4c73e
CubieProg <91529668+CubieProg@users.noreply.github.com>
2025-07-10 12:35:40 +0300

    Delete src/python test directory

--- src/python test/test.py
+++
@@ -1,9 +0,0 @@
-from yandex_tracker_client import TrackerClient
-
-client = TrackerClient(
-    token='y0_AgAEA7qkDtfXAAxntAAAAAEQXyAHAAAeCbMZsndMaYriBaGKrZ7yIU7G3Q', 
-    org_id='7091930'
-)
-
-
-print(client.issues['IIFA-8995'])
