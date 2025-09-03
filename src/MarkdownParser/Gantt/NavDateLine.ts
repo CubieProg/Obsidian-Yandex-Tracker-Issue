8a0da0136b29c3659f4976967933ce9ae5d7d288
Павел Соколов <spp1602@gmail.com>
2025-09-03 16:26:37 +0300

    Gantt dateNavLines removed

--- src/MarkdownParser/Gantt/NavDateLine.ts
+++ src/MarkdownParser/Gantt/NavDateLine.ts
@@ -1,28 +1,28 @@
 
 export const navDateLine = (date: Date, offset: number) => {
 
-    const dateNav = createDiv({
-        attr: {
-            class: "yti-gantt-navline-datenav",
-            style: `margin-left: ${offset}%;`
-        }
-    })
+    // const dateNav = createDiv({
+    //     attr: {
+    //         class: "gantt-navline-datenav",
+    //         style: `margin-left: ${offset}%;`
+    //     }
+    // })
 
-    const line = createDiv({ attr: { class: "yti-gantt-navline-line" } })
+    // const line = createDiv({ attr: { class: "gantt-navline-line" } })
 
-    const dateText = createDiv({ 
-        attr: { class: "yti-gantt-navline-text" }, 
-        text: date.toLocaleDateString()
-    })
+    // const dateText = createDiv({ 
+    //     attr: { class: "gantt-navline-text" }, 
+    //     text: date.toLocaleDateString()
+    // })
 
-    const wrapper = createDiv({ attr: { class: "yti-gantt-navline-wrapper" } })
+    // const wrapper = createDiv({ attr: { class: "gantt-navline-wrapper" } })
 
 
 
-    dateNav.appendChild(line)
-    dateNav.appendChild(dateText)
+    // dateNav.appendChild(line)
+    // dateNav.appendChild(dateText)
 
-    wrapper.appendChild(dateNav)
+    // wrapper.appendChild(dateNav)
 
-    return wrapper
+    // return wrapper
 }
